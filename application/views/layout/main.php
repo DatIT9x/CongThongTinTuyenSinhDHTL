e2e616ea85e42be917727c912b2173677321920e
LeXuanDatDeV <datlx62@wru.vn>
2020-05-29 13:02:31 +0700

    update

---
+++ application/views/layout/main.php
@@ -0,0 +1,97 @@
+<div id="main">
+    <div class="container">
+        <div id="main__wrapp">
+            <div id="main__left">
+                <div class="main__left--news">
+                    <h1 class="title__news">
+                        <p>Tin tuyển sinh 2020</p>
+                    </h1>
+                    <ul class="list__news">
+                        <li  class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                        <li class="list__news--item">
+                            <a href="">
+                                Đại học thủy lợi tổ chức buổi tư vấn trực tuyến ngày 30.5.2020
+                            </a>
+                            <p class="create__time">
+                                Thứ sáu,May 30,2020-10:12
+                            </p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div id="main__right">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/views/layout/slider.php
@@ -0,0 +1,8 @@
+<div id="slider">
+    <div class="carousel">
+        <a href="" class="image-item">
+            <img src="<?php echo base_url();?>asset/image/banner/1.jpg" alt="">
+        </a href="">
+        
+    </div>
+</div>
